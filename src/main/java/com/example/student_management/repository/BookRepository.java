b40a0847ae065f905998c770a2fa014959e99246
Giorgi <giorgimsxrikadze@gmail.com>
2026-08-02 23:04:37 +0400

    typo in import

--- src/main/java/com/example/student_management/repository/BookRepository.java
+++ src/main/java/com/example/student_management/repository/BookRepository.java
@@ -2,7 +2,7 @@ package com.example.student_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Book;
+import com.example.student_management.domain.Book;
 import java.util.UUID;
 
 public interface BookRepository extends JpaRepository<Book, UUID> {
