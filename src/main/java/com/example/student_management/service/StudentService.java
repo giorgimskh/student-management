f455394abf17791318d55f98d194c98b3b27514b
Giorgi <giorgimsxrikadze@gmail.com>
2026-08-03 18:32:29 +0400

    implemented assignBookToStudent method and added bookrepository and courserepository to handle database requests

--- src/main/java/com/example/student_management/service/StudentService.java
+++ src/main/java/com/example/student_management/service/StudentService.java
@@ -1,17 +1,28 @@
 package com.example.student_management.service;
 
 import com.example.student_management.domain.Student;
+import com.example.student_management.repository.BookRepository;
+import com.example.student_management.repository.CourseRepository;
 import com.example.student_management.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import com.example.student_management.domain.Book;
 
 @Service
 public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
+    private BookRepository bookRepository;
+    private CourseRepository courseRepository;
+
+    public StudentService(StudentRepository studentRepository, BookRepository bookRepository, CourseRepository courseRepository) {
+        this.studentRepository = studentRepository;
+        this.bookRepository = bookRepository;
+        this.courseRepository = courseRepository;
+    }
 
     public List<Student> getAllStudents(){
         return studentRepository.findAll();
@@ -34,5 +45,17 @@ public class StudentService {
 
     public void deleteStudent(UUID id){
         studentRepository.deleteById(id);
+    }
+
+    public Student assignBookToStudent(UUID studentId,UUID bookId){
+        Student student = getStudentById(studentId);
+        Book book =bookRepository.findById(bookId).orElseThrow(()->new RuntimeException("Book not found with id: " + bookId));
+
+        book.setStudent(student);
+        student.getBooks().add(book);
+
+        bookRepository.save(book);
+
+        return student;
     }
 }
