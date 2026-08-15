830c8b2b45b3d9ebc31799ac2efb34945810c6f8
Giorgi <giorgimsxrikadze@gmail.com>
2026-08-15 20:07:24 +0400

    implementeed new filter and added as bean in AppConfig

---
+++ src/main/java/com/example/student_management/RequestLoggingFilter.java
@@ -0,0 +1,33 @@
+package com.example.student_management;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import java.io.IOException;
+
+public class RequestLoggingFilter  implements Filter  {
+    //For writing Log Messages in console
+    //When messages are printed, they will be tagged with this class name, making it easy to see where the logs are coming from.
+    static private final Logger log= LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest=(HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        long startTime=System.currentTimeMillis();
+
+        log.info("[FILTER] Incoming Request: {} {}",httpServletRequest.getMethod(),httpServletRequest.getRequestURI());
+
+        try {
+            chain.doFilter(request,response);
+        } finally {
+            long duration = System.currentTimeMillis()-startTime;
+
+            log.info("[FILTER] Completed Request: {} {} status={}, duration={} ",httpServletRequest.getMethod(),httpServletRequest.getRequestURI(),httpServletResponse.getStatus(),duration);
+        }
+    }
+}
--- src/main/java/com/example/student_management/config/AppConfig.java
+++ src/main/java/com/example/student_management/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.example.student_management.config;
 
+import com.example.student_management.RequestLoggingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,16 @@ public class AppConfig {
     @Bean
     public Clock clock(){
         return Clock.systemUTC();
+    }
+
+    @Bean
+    public FilterRegistrationBean<RequestLoggingFilter> loggingFilter(){
+        FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new RequestLoggingFilter());
+
+        registrationBean.setOrder(1);
+
+        return registrationBean;
     }
 }
